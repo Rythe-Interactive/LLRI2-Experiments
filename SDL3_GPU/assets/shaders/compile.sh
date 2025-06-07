f99f5e849a8379aa7f0656cc365e7132209c504b
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-06-07 05:16:16 +0200

    Bash scripts formatting

--- SDL3_GPU/assets/shaders/compile.sh
+++ SDL3_GPU/assets/shaders/compile.sh
@@ -2,7 +2,6 @@
 
 # Requires the shadercross CLI to be installed from SDL_shadercross
 
-for file in *.hlsl ; do
+for file in *.hlsl; do
 	shadercross "$file" --Debug -o "compiled/${file/.hlsl/.spv}"
 done
-
--- Vulkan_Helpers/assets/shaders/compile.sh
+++ Vulkan_Helpers/assets/shaders/compile.sh
@@ -2,7 +2,6 @@
 
 # Requires glslangValidator to be installed from glslang
 
-for file in *.glsl ; do
+for file in *.glsl; do
 	glslangValidator --target-env vulkan1.3 -e main -o "compiled/${file/.glsl/.spv}" "$file"
 done
-
