c8255a60db22bf5f15718c45269b5290b1d13900
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-06-12 03:45:17 +0200

    SDL3_GPU: Fix compilation script

--- SDL3_GPU/assets/shaders/compile.sh
+++ SDL3_GPU/assets/shaders/compile.sh
@@ -3,5 +3,5 @@
 # Requires the shadercross CLI to be installed from SDL_shadercross
 
 for file in *.hlsl; do
-	shadercross "$file" --Debug -o "compiled/${file/.hlsl/.spv}"
+	shadercross "$file" --debug -o "compiled/${file/.hlsl/.spv}"
 done
